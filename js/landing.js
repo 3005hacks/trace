63beef1ab6b5b27cd57dec103f0901f474000855
therealdsmart <davidsmart21@gmail.com>
2015-11-20 23:22:12 -0500

    Landing Page Stage 1
    now we can do login functions

---
+++ js/landing.js
@@ -0,0 +1,18 @@
+function toggleID(element){
+	$('#' + 'element').toggle();
+}
+
+function showSignin(){
+	$('#signup-form').toggle();
+	$('#signin-form').toggle();
+	if($('#login-link').html() == "sign up"){
+		$('#login-link').html("sign in");
+	}
+	else if($('#login-link').html() == "sign in"){
+		$('#login-link').html("sign up");
+	}	
+}
+
+$('#sign-in').click( function(){
+	showSignin();
+});
