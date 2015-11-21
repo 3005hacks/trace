db178ecb2a2a2ad5496cb57bbaf70dd3049f6465
Joshua Bartlett <jcbart12595@gmail.com>
2015-11-21 07:52:12 -0500

    Uncommenting old functions

--- public/js/landing.js
+++ public/js/landing.js
@@ -2,20 +2,20 @@
 // 	$('#' + 'element').toggle();
 // }
 
-// function showSignin(){
-// 	$('#signup-form').toggle();
-// 	$('#signin-form').toggle();
-// 	if($('#login-link').html() == "sign up"){
-// 		$('#login-link').html("sign in");
-// 	}
-// 	else if($('#login-link').html() == "sign in"){
-// 		$('#login-link').html("sign up");
-// 	}	
-// }
+function showSignin(){
+	$('#signup-form').toggle();
+	$('#signin-form').toggle();
+	if($('#login-link').html() == "sign up"){
+		$('#login-link').html("sign in");
+	}
+	else if($('#login-link').html() == "sign in"){
+		$('#login-link').html("sign up");
+	}	
+}
 
-// $('#sign-in').click( function(){
-// 	showSignin();
-// });
+$('#sign-in').click( function(){
+	showSignin();
+});
 
 var currentUser = Parse.User.current();
 
